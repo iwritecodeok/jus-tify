f56c31091dec6526441691c588cb7ae9c28270bf
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-09-14 19:52:53 -0500

    login through spotify functional

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import './Styles/App.scss';
-import Login from './Components/Login';
+import React, { useEffect, useState } from "react";
+import "./Styles/App.scss";
+import Login from "./Components/Login";
+import { getTokenFromResponse } from "./Utils/spotify";
 
 function App() {
-  return (
-    <div className="app">
+  const [token, setToken] = useState(null);
 
-<Login/>
-{/* sign-up */}
-    </div>
-  );
+  useEffect(() => {
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    //_token naming convention for  temp
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+
+  return <div className="app">{token ? <h1>sick beats</h1> : <Login />}</div>;
 }
 
 export default App;
